784c634663f1df7673ee32f1116a64bafcf68023
Keerthana <132330274+keerthana89@users.noreply.github.com>
2023-05-04 07:45:17 -0700

    Add files via upload

---
+++ abcd.js
@@ -0,0 +1,22 @@
+window.onscroll = () =>{
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-top').classList.add('active');
+        
+    } else {
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
+function myFunction() {
+    alert("Order Placed Successfully");
+    window.location = "abcd.html";
+  }
